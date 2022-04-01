f423e7faa8ee300049a18b67d7084f1b17a72ad2
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-01 16:26:25 +0200

    Add Task

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-
+            jobs:[
+                {
+                    message: 'Portare a passeggio il cane',
+                    done: 'False'
+                },
+                {
+                    message: 'Buttare la Spazzatura',
+                    done: 'False'
+                },
+                {
+                    message: 'Svuotare la lavastoviglie',
+                    done: 'False'
+                }
+            ],
+            toDo: ' ',
         },
         methods: {
-
+            addJob: function() {
+                let jobObj = {
+                    message: this.toDo,
+                    done: 'false'
+                }
+                console.log(jobObj);
+                this.jobs.push(jobObj);
+            }
         }
     }
 )
