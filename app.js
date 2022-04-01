aaee7ee6188fe91ef005861e9e72e477acbf731a
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-01 16:43:57 +0200

    delete function

--- app.js
+++ app.js
@@ -16,16 +16,25 @@ var app = new Vue (
                     done: 'False'
                 }
             ],
+            //istanzio una variabile nuova che dipende dal v-model in input
             toDo: ' ',
         },
         methods: {
+            //creo funzione per aggiungere in jobs l'imput messo
             addJob: function() {
+                //dato il messaggio nell'imput creo un oggetto da inserire nell'array in dati
                 let jobObj = {
                     message: this.toDo,
                     done: 'false'
                 }
                 console.log(jobObj);
+                //pusho l'oggetto in jobs
                 this.jobs.push(jobObj);
+            },
+            //creo funzione per eliminare la task
+            //il parametro inserito nella funzione è l'index dell'oggetto nell'array preso da html
+            delJob: function(jobPosition){
+                this.jobs.splice(jobPosition,1);
             }
         }
     }
